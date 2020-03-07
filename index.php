d25c90d681bf0945b3b592aebd903a5e9c9dff3a
Narek <58231869+Narek-ava@users.noreply.github.com>
2020-03-07 15:46:06 +0400

    add CodeSchool

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CodeSchools</title>
+</head>
+<body>
+ 
+    
+   
+    <br> <a href="less2.php">
+        less2
+    </a><br>
+    <a href="less3.php">
+        less3
+    </a>
+   <br> <a href="less4.php">
+        less4
+    </a>
+     <br> <a href="less5.php">
+        less5
+    </a>
+    <br> <a href="less6.php">
+        less6
+    </a>
+    <br> <a href="less7.php">
+        less7
+    </a>
+     <br> <a href="less8.php">
+        less8
+    </a>
+     <p1>   
+         <br> <a href="less9.php">
+        less9
+    </a><br>
+     <p1>
+          <br> <a href="less10.php">
+             less10
+         </a><br>
+(ռուսերեն)<br>
+<br>1////////////////////////////////////////////////////////////////////////////////////<br>
+<br>https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number
+<br>https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String
+<br>https://developer.mozilla.org/ru/docs/Web/JavaScript/Guide/Expressions_and_Operators
+<br>2////////////////////////////////////////////////////////////////////////////////////<br>
+<br>https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Statements/if...else
+<br>https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Statements/switch
+<br>3///////////////////////////////////////////////////////////////////////////////////<br>
+<br>https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Statements/for
+<br>4///////////////////////////////////////////////////////////////////////////////////<br>
+<br>https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Statements/while
+<br>https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Statements/do...while
+<br>5///////////////////////////////////////////////////////////////////////////////////<br>
+<br>https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array
+<br>6///////////////////////////////////////////////////////////////////////////////////<br>
+<br>https://learn.javascript.ru/object    -   մինչև «Свойство из переменной» ենթավերնագիր,
+<br>https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Object/keys,
+<br>https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Object/values
+<br>https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+<br>7///////////////////////////////////////////////////////////////////////////////////<br>
+<br>https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+<br>https://learn.javascript.ru/function-basics
+<br>8///////////////////////////////////////////////////////////////////////////////////<br>
+<br>https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+<br>https://developer.mozilla.org/ru/docs/Web/JavaScript/Closures
+<br>https://learn.javascript.ru/recursion
+<br>9///////////////////////////////////////////////////////////////////////////////////<br>
+<br>///////////////////////////////////////////////////////////////////////////////////<br><br>
+<br>10///////////////////////////////////////////////////////////////////////////////////<br><br>
+Տեսական հատվածը կարդալ հետևյալ հղումներով.
+https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+</p1>
+</body>
+<script>
+
+</script>
+</html>
+
